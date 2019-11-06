05528ebc3f7c9f1ddce183dab07f96f46bc29819
Alexander <alternativevisualgames@gmail.com>
2019-11-06 19:42:36 +0700

    Update AnimatorHash.cs

--- AnimatorHash/AnimatorHash.cs
+++ AnimatorHash/AnimatorHash.cs
@@ -163,20 +163,23 @@ static class AnimatorHashExtension
     }
 }
 #if UNITY_EDITOR
-[CustomPropertyDrawer(typeof(AnimatorHash))]
-public class AnimatorHashDrawer : PropertyDrawer
+namespace Neonagee.Editor.PropertyDrawers 
 {
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    [CustomPropertyDrawer(typeof(AnimatorHash))]
+    public class AnimatorHashDrawer : PropertyDrawer
     {
-        var propertyName = property.FindPropertyRelative("property");
-        EditorGUI.BeginProperty(position, label, property);
-        EditorGUI.BeginChangeCheck();
-        propertyName.stringValue = EditorGUI.TextField(position, label, propertyName.stringValue);
-        if (EditorGUI.EndChangeCheck())
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            property.FindPropertyRelative("hash").intValue = Animator.StringToHash(propertyName.stringValue);
+            var propertyName = property.FindPropertyRelative("property");
+            EditorGUI.BeginProperty(position, label, property);
+            EditorGUI.BeginChangeCheck();
+            propertyName.stringValue = EditorGUI.TextField(position, label, propertyName.stringValue);
+            if (EditorGUI.EndChangeCheck())
+            {
+                property.FindPropertyRelative("hash").intValue = Animator.StringToHash(propertyName.stringValue);
+            }
+            EditorGUI.EndProperty();
         }
-        EditorGUI.EndProperty();
     }
 }
 #endif
