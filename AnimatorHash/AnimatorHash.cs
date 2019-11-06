9576679af658a1e99b26df04fe8045eca0220389
Alexander <alternativevisualgames@gmail.com>
2019-11-06 14:52:51 +0300

    Initial commit

---
+++ AnimatorHash/AnimatorHash.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[System.Serializable]
+public struct AnimatorHash
+{
+    public string property;
+    public int hash;
+    public AnimatorHash(string propertyName)
+    {
+        property = propertyName;
+        hash = Animator.StringToHash(property);
+    }
+}
+static class AnimatorHashExtension
+{
+    public static bool IsCurrentState(this Animator animator, AnimatorHash stateNameHash, int layerIndex = 0)
+    {
+        return animator.GetCurrentAnimatorStateInfo(layerIndex).shortNameHash == stateNameHash.hash;
+    }
+    public static bool IsCurrentState(this Animator animator, AnimatorHash stateNameHash, out AnimatorStateInfo stateInfo, int layer = 0)
+    {
+        stateInfo = animator.GetCurrentAnimatorStateInfo(layer);
+        return stateInfo.shortNameHash == stateNameHash.hash;
+    }
+
+    public static void Play(this Animator animator, AnimatorHash stateNameHash)
+    {
+        animator.Play(stateNameHash.hash);
+    }
+    public static void Play(this Animator animator, AnimatorHash stateNameHash, int layer)
+    {
+        animator.Play(stateNameHash.hash, layer);
+    }
+    /// <summary>
+    /// Plays a state.
+    /// </summary>
+    /// <param name="stateNameHash">The state hash name. If stateNameHash is 0, it changes the current state time.</param>
+    /// <param name="layer">The layer index. If layer is -1, it plays the first state with the given state name or hash.</param>
+    /// <param name="normalizedTime">The time offset between zero and one.</param>
+    public static void Play(this Animator animator, AnimatorHash stateNameHash, int layer = -1, float normalizedTime = float.NegativeInfinity)
+    {
+        animator.Play(stateNameHash.hash, layer, normalizedTime);
+    }
+
+    /// <summary>
+    /// Plays a state.
+    /// </summary>
+    /// <param name="stateNameHash">The state hash name. If stateNameHash is 0, it changes the current state time.</param>
+    /// <param name="layer">The layer index. If layer is -1, it plays the first state with the given state name or hash.</param>
+    /// <param name="fixedTime">The time offset (in seconds).</param>
+    public static void PlayInFixedTime(this Animator animator, AnimatorHash stateNameHash, int layer, float fixedTime)
+    {
+        animator.PlayInFixedTime(stateNameHash.hash, layer, fixedTime);
+    }
+    public static void PlayInFixedTime(this Animator animator, AnimatorHash stateNameHash)
+    {
+        animator.PlayInFixedTime(stateNameHash.hash);
+    }
+    public static void PlayInFixedTime(this Animator animator, AnimatorHash stateNameHash, int layer)
+    {
+        animator.PlayInFixedTime(stateNameHash.hash, layer);
+    }
+
+    //
+    // Сводка:
+    //     Plays a state.
+    //
+    // Параметры:
+    //   stateName:
+    //     
+    //
+    //   stateNameHash:
+    //     The state hash name. If stateNameHash is 0, it changes the current state time.
+    //
+    //   layer:
+    //     The layer index. If layer is -1, it plays the first state with the given state
+    //     name or hash.
+    //
+    //   normalizedTime:
+    //     The time offset between zero and one.
+
+
+    /// <summary>
+    /// Sets the value of the given float parameter.
+    /// </summary>
+    /// <param name="id">The parameter ID.</param>
+    /// <param name="value">The new parameter value.</param>
+    public static void SetFloat(this Animator animator, AnimatorHash id, float value)
+    {
+        animator.SetFloat(id.hash, value);
+    }
+    /// <summary>
+    /// Send float values to the Animator to affect transitions.
+    /// </summary>
+    /// <param name="id">The parameter ID.</param>
+    /// <param name="value">The new parameter value.</param>
+    /// <param name="dampTime">The damper total time.</param>
+    /// <param name="deltaTime">The delta time to give to the damper.</param>
+    public static void SetFloat(this Animator animator, AnimatorHash id, float value, float dampTime, float deltaTime)
+    {
+        animator.SetFloat(id.hash, value, dampTime, deltaTime);
+    }
+    /// <summary>
+    /// Sets the value of the given integer parameter.
+    /// </summary>
+    /// <param name="id">The parameter ID.</param>
+    /// <param name="value">The new parameter value.</param>
+    public static void SetInteger(this Animator animator, AnimatorHash id, int value)
+    {
+        animator.SetInteger(id.hash, value);
+    }
+    /// <summary>
+    /// Sets the value of the given boolean parameter.
+    /// </summary>
+    /// <param name="id">The parameter ID.</param>
+    /// <param name="value">The new parameter value.</param>
+    public static void SetBool(this Animator animator, AnimatorHash id, bool value)
+    {
+        animator.SetBool(id.hash, value);
+    }
+    /// <summary>
+    /// Sets the value of the given quaternion parameter.
+    /// </summary>
+    /// <param name="id">The parameter ID.</param>
+    /// <param name="value">The new parameter value.</param>
+    [System.Obsolete]
+    public static void SetQuaternion(this Animator animator, AnimatorHash id, Quaternion value)
+    {
+        animator.SetQuaternion(id.hash, value);
+    }
+    /// <summary>
+    /// Sets the value of the given vector parameter.
+    /// </summary>
+    /// <param name="id">The parameter ID.</param>
+    /// <param name="value">The new parameter value.</param>
+    [System.Obsolete]
+    public static void SetVector(this Animator animator, AnimatorHash id, Vector3 value)
+    {
+        animator.SetVector(id.hash, value);
+    }
+    /// <summary>
+    /// Sets the value of the given trigger parameter.
+    /// </summary>
+    /// <param name="id">The parameter ID.</param>
+    /// <param name="value">The new parameter value.</param>
+    public static void SetTrigger(this Animator animator, AnimatorHash id)
+    {
+        animator.SetTrigger(id.hash);
+    }
+    /// <summary>
+    /// Resets the value of the given trigger parameter.
+    /// </summary>
+    /// <param name="id">The parameter ID.</param>
+    /// <param name="value">The new parameter value.</param>
+    public static void ResetTrigger(this Animator animator, AnimatorHash id)
+    {
+        animator.ResetTrigger(id.hash);
+    }
+}
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(AnimatorHash))]
+public class AnimatorHashDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        var propertyName = property.FindPropertyRelative("property");
+        EditorGUI.BeginProperty(position, label, property);
+        EditorGUI.BeginChangeCheck();
+        propertyName.stringValue = EditorGUI.TextField(position, label, propertyName.stringValue);
+        if (EditorGUI.EndChangeCheck())
+        {
+            property.FindPropertyRelative("hash").intValue = Animator.StringToHash(propertyName.stringValue);
+        }
+        EditorGUI.EndProperty();
+    }
+}
+#endif
---
+++ AnimatorHash/Test/AnimatorHashTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Neonagee.Tests
+{
+    public class AnimatorHashTest : MonoBehaviour
+    {
+        public Animator animator;
+        public ParticleSystem shotParticle;
+        [Space]
+        public float fireTime = 0.3f;
+        float m_fireTime;
+        [Space]
+        public AnimatorHash fireAnim = new AnimatorHash("Fire");
+        public AnimatorHash moveAnim = new AnimatorHash("Move");
+        public AnimatorHash jumpAnim = new AnimatorHash("Jump");
+        public AnimatorHash horizontalParam = new AnimatorHash("Horizontal");
+        public AnimatorHash verticalParam = new AnimatorHash("Vertical");
+
+        private void Update()
+        {
+            var HAxis = Input.GetAxis("Horizontal");
+            var VAxis = Input.GetAxis("Vertical");
+
+            if (Input.GetMouseButton(0) && Time.time >= m_fireTime)
+            {
+                animator.Play(fireAnim, 1, 0);
+                shotParticle.Play();
+                m_fireTime = Time.time + fireTime;
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+                animator.Play(jumpAnim);
+
+            if (HAxis != 0 || VAxis != 0)
+            {
+                animator.Play(moveAnim);
+                animator.SetFloat(horizontalParam, HAxis);
+                animator.SetFloat(verticalParam, VAxis);
+            }
+        }
+    }
+}
