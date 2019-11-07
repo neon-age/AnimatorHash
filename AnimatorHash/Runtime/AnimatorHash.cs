e5e982fe6154d4a5beb3b066a51cab35eee2acce
Alexander <alternativevisualgames@gmail.com>
2019-11-07 16:57:35 +0700

    Update AnimatorHash.cs

--- AnimatorHash/Runtime/AnimatorHash.cs
+++ AnimatorHash/Runtime/AnimatorHash.cs
@@ -2,35 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Neonagee
+[System.Serializable]
+public struct AnimatorHash
 {
-    [System.Serializable]
-    public struct AnimatorHash
+    public string property;
+    public int hash;
+    public AnimatorHash(string propertyName)
     {
-        public string property;
-        public int hash;
-        public AnimatorHash(string propertyName)
-        {
-            property = propertyName;
-            hash = Animator.StringToHash(property);
-        }
-
-        public static implicit operator int(AnimatorHash animatorHash)
-        {
-            return animatorHash.hash;
-        }
+        property = propertyName;
+        hash = Animator.StringToHash(property);
     }
 
-    public static class AnimatorHashExtension
+    public static implicit operator int(AnimatorHash animatorHash)
     {
-        public static bool IsCurrentState(this Animator animator, AnimatorHash stateNameHash, int layerIndex = 0)
-        {
-            return animator.GetCurrentAnimatorStateInfo(layerIndex).shortNameHash == stateNameHash.hash;
-        }
-        public static bool IsCurrentState(this Animator animator, AnimatorHash stateNameHash, out AnimatorStateInfo stateInfo, int layer = 0)
-        {
-            stateInfo = animator.GetCurrentAnimatorStateInfo(layer);
-            return stateInfo.shortNameHash == stateNameHash.hash;
-        }
+        return animatorHash.hash;
+    }
+}
+
+public static class AnimatorHashExtension
+{
+    public static bool IsCurrentState(this Animator animator, AnimatorHash stateNameHash, int layerIndex = 0)
+    {
+        return animator.GetCurrentAnimatorStateInfo(layerIndex).shortNameHash == stateNameHash.hash;
+    }
+    public static bool IsCurrentState(this Animator animator, AnimatorHash stateNameHash, out AnimatorStateInfo stateInfo, int layer = 0)
+    {
+        stateInfo = animator.GetCurrentAnimatorStateInfo(layer);
+        return stateInfo.shortNameHash == stateNameHash.hash;
     }
 }
